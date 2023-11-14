79797c49d85a0658f6e19116490c0851085033cf
Robert Leung <rleung84@uwo.ca>
2023-11-13 21:46:40 -0500

    updates to bfs logic

--- knights_travails.js
+++ knights_travails.js
@@ -6,7 +6,7 @@ for (let i = 0; i < boardSize; i++) {
     gameBoard.push([i, j]);
   }
 }
-// console.log(gameBoard);
+console.log(gameBoard);
 
 const adjacencyList = [];
 
@@ -25,10 +25,15 @@ function validMoves(pos) {
   const moves = knightMoves
     .filter((knightMove) => {
       // console.log(pos[0] + knightMove[0] >= 0 && pos[1] + knightMove[1] >= 0);
-      return pos[0] + knightMove[0] >= 0 && pos[1] + knightMove[1] >= 0;
+      return (
+        pos[0] + knightMove[0] >= 0 &&
+        pos[0] + knightMove[0] < 8 &&
+        pos[1] + knightMove[1] >= 0 &&
+        pos[1] + knightMove[1] < 8
+      );
     })
     .map((move) => {
-      return [pos[0] + move[0], pos[1] + move[1]];
+      return [(pos[0] + move[0]) * 8 + (pos[1] + move[1])];
     });
   return moves;
 }
@@ -38,4 +43,46 @@ for (let i = 0; i < gameBoard.length; i++) {
 }
 
 // console.log(validMoves([0, 0]));
-console.log(adjacencyList);
+// for (let i = 0; i < adjacencyList.length; i++) {
+//   for (let j = 0; j < adjacencyList[i].length; j++) {
+//     if (adjacencyList[i][j] > 63) {
+//       console.log(i);
+//       console.log(j);
+//       console.log(adjacencyList[i][j]);
+//     }
+//   }
+// }
+// console.log(adjacencyList);
+// console.log(adjacencyList[63]);
+
+function bfs(start) {
+  const queue = [start];
+  const visited = new Set();
+  const result = [];
+
+  while (queue.length) {
+    const vertex = queue.shift();
+    // console.log(queue.length);
+    // console.log(visited);
+
+    if (!visited.has(vertex)) {
+      visited.add(vertex);
+      result.push(vertex);
+
+      //   console.log("---------------");
+      //   console.log(queue);
+      //   console.log(visited);
+      //   console.log(vertex);
+      //   console.log(result);
+      //   console.log(adjacencyList[vertex]);
+      //   console.log(vertex);
+      for (const neighbor of adjacencyList[vertex]) {
+        queue.push(neighbor);
+      }
+    }
+  }
+
+  return result;
+}
+
+console.log(bfs([0]));
